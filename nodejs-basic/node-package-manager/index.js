e09b614c46da90f324eb4140e9f27abf2ec14785
reavywilldan <reavywilldan@gmail.com>
2022-07-16 11:23:36 +0700

    add confic package json

--- nodejs-basic/node-package-manager/index.js
+++ nodejs-basic/node-package-manager/index.js
@@ -1,5 +1,5 @@
-const _ = require('lodash')
+import { partition } from 'lodash';
  
-const myOddEvenArray = _.partition([1, 2, 3, 4, 5, 6], (n) => n % 2);
+const myOddEvenArray = partition([1, 2, 3, 4, 5, 6], (n) => n % 2);
  
 console.log(myOddEvenArray);
